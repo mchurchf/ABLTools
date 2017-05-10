e95fcd0224355301e5bccb216fa00e9c76d0c5a7
Churchfield <mchurchf@mchurchf-24212s.nrel.gov>
2017-05-10 07:46:07 -0600

    Some reorganization of files to match Paula's organization.

--- python/driverExample.py
+++ python/driverExample.py
@@ -14,6 +14,7 @@
 import getDataLayout
 import readData
 from statistics import timeStatistics
+from statistics import spatialCorrelations
 
 import matplotlib
 import matplotlib.cm as cm
@@ -34,6 +35,9 @@ meanTimeStart = 10400.0
 
 # Variance start time.
 varianceTimeStart = 11400.0
+
+# Spatial correlation start time.
+spatialCorrelationTimeStart = 11400.0
 
 # For plotting.
 nLevels = 51;
@@ -66,21 +70,27 @@ default_cmap = plt.cm.viridis
 
 
 # Get the data directory layout.
-nTimes, outputTimes = getDataLayout.getOutputTimes(dir)
+nTimes, outputTimes = getDataLayout.getOutputTimes(case)
 
 
 # Read in the first sample of velocity so that we can set up the basic time statistics.
-fileName = case + '/' + outputTimes[m] + '/' + 'array.1_U.vtk'
+fileName = case + '/' + outputTimes[0] + '/' + 'array.1_U.vtk'
 dataSetName, dims, origin, spacing, x, y, z, nFields, fieldName, fieldDim, field = readData.structuredVTK(fileName)
 
 
 # Reshape the velocity data into a plane (i.e. remove the x-axis because data
 # here is in y and z.
-u = np.array(field[0][:,0,:,:,].reshape((fieldDim[0],dims[2],dims[1])))
+#u = np.array(field[0][:,0,:,:,].reshape((fieldDim[0],dims[2],dims[1])))
+u = np.array(field[0])
 
 
 # Create the time statistics class object.
 uTimeStatistics = timeStatistics(u)
+#uTimeStatistics = timeStatistics(field[0])
+
+
+# Create the spatial correlations statistics class object.
+uSpatialCorrelations = spatialCorrelations(u,x,y,z,1500.0,1000.0,99.72)
 
 
 # Loop over time and accumuate statistics, but disregard anything before the time at which
@@ -105,16 +115,23 @@ for m in range(nTimes):
     
         # Reshape the data into a plane (i.e. remove the x-axis because data here
         # is in y and z.
-        u = np.array(field[0][:,0,:,:,].reshape((fieldDim[0],dims[2],dims[1])))
+        #u = np.array(field[0][:,0,:,:,].reshape((fieldDim[0],dims[2],dims[1])))
+        u = np.array(field[0])
     
     
         # Acculate the mean.
         uTimeStatistics.accumulateMean(u,dt)
+    
 
 
         # Accumulate the variance.
         if (float(outputTimes[m]) >= varianceTimeStart):
-            uTimeStatistics.accumulateVariance(u,dt)
+            uTimeStatistics.accumulateVariance(u,uTimeStatistics.meanField,dt)
+            
+        
+        # Accumulate the spatial correlations.
+        if (float(outputTimes[m]) >= spatialCorrelationTimeStart):
+            uSpatialCorrelations.accumulateCorrelation(u,uTimeStatistics.meanField,dt)
             
 
 print 'Computed statistics with:'
@@ -124,19 +141,20 @@ print '   ' + str(uTimeStatistics.varianceAccumulatedTime) + 's of variance accu
 
 
 # Plot an instantaneous slice of velocity
-u = u[0,:,:];
+#u = u[0,:,:];
+u = u[0,0,:,:].reshape((dims[2],dims[1]))
 plt.figure(1)
 plt.contourf(y,z,u,nLevels,cmap=default_cmap)
 plt.gca().set_aspect('equal', adjustable='box')
 cbar = plt.colorbar(shrink=0.6, extend='both')
-cbar.ax.set_ylabel('$u_x' + ' (m/s)',fontweight='bold')
+cbar.ax.set_ylabel('$u_x$' + ' (m/s)',fontweight='bold')
 plt.xlabel('y (m)',fontweight='bold')
 plt.ylabel('z (m)',fontweight='bold')
 plt.tight_layout()
 
 
 # Plot the mean velocity.
-uAvg = uTimeStatistics.meanField[0,:];
+uAvg = uTimeStatistics.meanField[0,:].reshape((dims[2],dims[1]));
 plt.figure(2)
 plt.contourf(y,z,uAvg,nLevels,cmap=default_cmap)
 plt.gca().set_aspect('equal', adjustable='box')
@@ -147,12 +165,13 @@ plt.ylabel('z (m)',fontweight='bold')
 plt.tight_layout()
 
 
+
 # Plot the variance field.  The components map like this;
 # 0 = xx, 1 = xy, 2 = xz
 # 3 = yx, 4 = yy, 5 = yz
 # 6 = zx, 7 = zy, 8 = zz
 cmpt = 2
-var = uTimeStatistics.varianceField[cmpt,:];
+var = uTimeStatistics.varianceField[cmpt,:].reshape((dims[2],dims[1]));
 plt.figure(3)
 plt.contourf(y,z,var,nLevels,cmap=default_cmap)
 plt.gca().set_aspect('equal', adjustable='box')
@@ -164,12 +183,41 @@ plt.tight_layout()
 
 
 cmpt = 0
-var = uTimeStatistics.varianceField[cmpt,:];
+var = uTimeStatistics.varianceField[cmpt,:].reshape((dims[2],dims[1]));
 plt.figure(4)
 plt.contourf(y,z,var,nLevels,cmap=default_cmap)
 plt.gca().set_aspect('equal', adjustable='box')
 cbar = plt.colorbar(shrink=0.6, extend='both')
 cbar.ax.set_ylabel('$\overline{u\'_x u\'_x}$' + ' (m/s)',fontweight='bold')
+plt.xlabel('y (m)',fontweight='bold')
+plt.ylabel('z (m)',fontweight='bold')
+plt.tight_layout()
+
+
+
+# Plot the spatial correlation field.  The components map like this;
+# 0 = xx, 1 = xy, 2 = xz
+# 3 = yx, 4 = yy, 5 = yz
+# 6 = zx, 7 = zy, 8 = zz
+cmpt = 2
+var = uSpatialCorrelations.correlationField[cmpt,:].reshape((dims[2],dims[1]));
+plt.figure(5)
+plt.contourf(y,z,var,nLevels,cmap=default_cmap)
+plt.gca().set_aspect('equal', adjustable='box')
+cbar = plt.colorbar(shrink=0.6, extend='both')
+cbar.ax.set_ylabel('$C_{xz}$' + ' (m/s)',fontweight='bold')
+plt.xlabel('y (m)',fontweight='bold')
+plt.ylabel('z (m)',fontweight='bold')
+plt.tight_layout()
+
+
+cmpt = 0
+var = uSpatialCorrelations.correlationField[cmpt,:].reshape((dims[2],dims[1]));
+plt.figure(6)
+plt.contourf(y,z,var,nLevels,cmap=default_cmap)
+plt.gca().set_aspect('equal', adjustable='box')
+cbar = plt.colorbar(shrink=0.6, extend='both')
+cbar.ax.set_ylabel('$C_{xx}$' + ' (m/s)',fontweight='bold')
 plt.xlabel('y (m)',fontweight='bold')
 plt.ylabel('z (m)',fontweight='bold')
 plt.tight_layout()
--- python/statistics.py
+++ python/statistics.py
@@ -44,37 +44,12 @@ class timeStatistics:
         
         
     # accumulate the variance tensor using the current estimate of the mean.
-    def accumulateVariance(self,fieldUpdate,dt):
-        # take the current estimate of the variances and multiply by the
-        # previous total averaging time
-        self.varianceField = self.varianceField * self.varianceAccumulatedTime
-        
-        # subtract out the current mean field from the new contribution field.
-        fluctuationField = fieldUpdate - self.meanField
-        
-        # Add dt to the total averaging time.
-        self.varianceAccumulatedTime = self.varianceAccumulatedTime + dt
-        
-        # Compute the new variance field contributions. Add the new variance 
-        # contribution onto the current variance weighted by its dt, and divide
-        # by the new total averaging time.
-        for i in range(self.componentsMean):
-            for j in range(self.componentsMean):
-                v = np.multiply(fluctuationField[i,:],fluctuationField[j,:])
-                index = (i*self.componentsMean) + j
-                self.varianceField[index,:] = self.varianceField[index,:] + (dt * v)
-                
-        self.varianceField = self.varianceField / self.varianceAccumulatedTime
-        
-        
-        
-    # accumulate the variance tensor using a given mean.
     def accumulateVariance(self,fieldUpdate,fieldMean,dt):
         # take the current estimate of the variances and multiply by the
         # previous total averaging time
         self.varianceField = self.varianceField * self.varianceAccumulatedTime
         
-        # subtract out the mean field from the new contribution field.
+        # subtract out the given mean field from the new contribution field.
         fluctuationField = fieldUpdate - fieldMean
         
         # Add dt to the total averaging time.
@@ -93,7 +68,70 @@ class timeStatistics:
         
         
         
-# class spatialCorrelations:
+
+        
+        
+        
+class spatialCorrelations:
+    # intitialize the class
+    def __init__(self,field,x,y,z,x0,y0,z0):
+        dimsField = np.asarray(field.shape)
+        self.componentsField = dimsField[0]
+        self.componentsCorrelation = self.componentsField**2
+        dimsCorrelation = 1*dimsField
+        dimsCorrelation[0] = self.componentsCorrelation
+        
+        self.correlationField = np.zeros(dimsCorrelation)
+        self.correlationAccumulatedTime = 0.0
+        
+        self.x = x
+        self.y = y
+        self.z = z
+        
+        
+        # find the point nearest (x0,y0,z0)
+        minDis = 1.0E10
+        self.iNearest = -1
+        self.jNearest = -1
+        self.kNearest = -1
+        for i in range(dimsField[1]):
+            for j in range(dimsField[2]):
+                for k in range(dimsField[3]):
+                    dx = x[i] - x0;
+                    dy = y[j] - y0;
+                    dz = z[k] - z0;
+                    dis = (dx**2 + dy**2 + dz**2)**0.5
+                    if (dis < minDis):
+                        minDis = dis
+                        self.iNearest = i 
+                        self.jNearest = j
+                        self.kNearest = k
+                        
+        print 'Nearest = (' + str(x[self.iNearest]) + ', ' + str(y[self.jNearest]) + ', ' + str(z[self.kNearest]) + ')'
+        print 'Indes = (' + str(self.iNearest) + ', ' + str(self.jNearest) + ', ' + str(self.kNearest) + ')'
+        
+        
+    # accumulate the spatial correlation tensor field.    
+    def accumulateCorrelation(self,fieldUpdate,fieldMean,dt):
+        # take the current estimate of the variances and multiply by the
+        # previous total averaging time
+        self.correlationField = self.correlationField * self.correlationAccumulatedTime
+        
+        # subtract out the given mean field from the new contribution field.
+        fluctuationField = fieldUpdate - fieldMean
+        
+        # Add dt to the total averaging time.
+        self.correlationAccumulatedTime = self.correlationAccumulatedTime + dt
+        
+        # Get the correlations
+        for i in range(self.componentsField):
+            for j in range(self.componentsField):
+                v = fluctuationField[i,:] * fluctuationField[j,self.iNearest,self.jNearest,self.kNearest]
+                index = (i*self.componentsField) + j
+                self.correlationField[index,:] = self.correlationField[index,:] + (dt * v)
+                
+        self.correlationField = self.correlationField / self.correlationAccumulatedTime
+    
 # I envision a class here to do spatial correlations.  Maybe you give it the 
 # location which is the center point of the correlation.  Like, you give it
 # the center of your data set, and then it loops through all points and does
@@ -109,3 +147,41 @@ class timeStatistics:
 # history of velocity along the whole span of a sampling plane.  It would be 
 # nice, too, if when you give it a vector time history, it returns a tensor of
 # spectra.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
